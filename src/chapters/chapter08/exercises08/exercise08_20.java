7aa25f4fb4ab829f2f49e7fef419df67e78bd9a5
etkaberk <etkaberk96@gmail.com>
2022-10-23 18:14:41 +0300

    keep going with chapter8

---
+++ src/chapters/chapter08/exercises08/exercise08_20.java
@@ -0,0 +1,17 @@
+package chapters.chapter08.exercises08;
+// - 6 satrı 7 sütunluk tahta
+// - (4 disk)aynı satır -aynı situn -capraz sağ/sol
+// - oyuncular sırayla taş atıyor
+// validation check yapıcaz girilen sayıyı kontrol
+//sütundan küçük giriş yapılmalı
+// dolu sütuna atılmamalı
+//diplay metodu --> anlık bordu gösterelim
+// user ın girdiği değeri borda ata
+// kazanma yada berabere durumu var mı?
+//
+public class exercise08_20{
+    public static void main(String[] args) {
+
+    }
+
+}
