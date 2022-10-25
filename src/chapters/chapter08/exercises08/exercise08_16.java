cd273e5aedf3a71e29a8bc5db0a2a76ad40bb6c3
etkaberk <etkaberk96@gmail.com>
2022-10-25 08:19:34 +0300

    last works

--- src/chapters/chapter08/exercises08/exercise08_16.java
+++ src/chapters/chapter08/exercises08/exercise08_16.java
@@ -1,4 +1,46 @@
 package chapters.chapter08.exercises08;
 
 public class exercise08_16 {
+    public static void main(String[] args) {
+        int[][] list = {{4, 2}, {1, 7}, {4, 5}, {1, 2}, {1, 1}, {4, 1}};
+        displayList(list);
+
+        sort(list);
+        displayList(list);
+    }
+
+    public static void sort(int[][] list) {
+        int point ;
+        for (int i = 0; i < list.length - 1; i++) {
+            point = i;
+            for (int j = i + 1; j < list.length; j++) {
+
+                if (list[j][0] < list[point][0]) {
+                    point = j;
+                }
+                else if (list[j][0] == list[point][0]) {
+                    if (list[j][1] < list[i][1]) {
+                        point = j;
+                    }
+                }
+            }
+            if(point != i){
+
+                int temp = list[i][0];
+                list[i][0] = list[point][0];
+                list[point][0] = temp;
+
+                temp = list[i][1];
+                list[i][1] = list[point][1];
+                list[point][1] = temp;
+            }
+        }
+    }
+
+    public static void displayList(int[][] list) {
+        for (int[] point : list) {
+            System.out.print("(" + point[0] + ", " + point[1] + ")  ");
+        }
+        System.out.println();
+    }
 }
