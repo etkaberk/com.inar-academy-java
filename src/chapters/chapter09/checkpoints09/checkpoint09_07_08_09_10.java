9c5be896d1a1026e633e1a93b54d28d5bdb0c8b0
etkaberk <etkaberk96@gmail.com>
2022-10-31 20:09:42 +0300

    new checkpoints

---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_07_08_09_10.java
@@ -0,0 +1,13 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_07_08_09_10 {
+    /**
+     * 9.7--> (.) operator is used for accessing a data field or invoke a method from an object
+     * 9.8--> if an object is not assigned to  a variable , it is an anonymous object.
+     * 9.9--> when we invoke a method by null referenced variable it will caused NullPointerException!
+     * 9.10--> array is an object and it can contains objects too.
+     * for the numerical variables it is '0'
+     * for the String it is 'null'
+     * for the char it is 'u\0000
+     */
+}
