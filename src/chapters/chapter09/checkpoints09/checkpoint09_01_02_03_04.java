2aaf79607546ca358221407f22f84bd7b27d41e7
etkaberk <etkaberk96@gmail.com>
2022-10-31 17:03:00 +0300

    added some checkpoint

---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_01_02_03_04.java
@@ -0,0 +1,9 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_01_02_03_04 {
+    // 9.1 --> objects are the instance of the class and we can define the class as a entire objects
+    //9.2 --> a class is a template.Class defines what an objects data fields and methods are
+    //9.3 --> we declare an object's reference variable with the class name
+    //9.4 --> as ;  ClassName instanceVariableName = new ClassName()
+
+}
---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_05_06.java
@@ -0,0 +1,11 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_05_06 {
+    /** 9.5 --> constructors must have the same name as the class
+     * -constructors do not have return type not even void
+     * -constructors are invoked using the new operator
+     * it play the role of initializing objects
+     */
+// 9.6-->when u not write a constructor java will add it (default constructor)
+
+}
