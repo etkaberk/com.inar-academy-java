4a454910a29d56aefa85c020802916a232f16958
etkaberk <etkaberk96@gmail.com>
2022-11-01 16:49:37 +0300

    new listing

---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_13.java
@@ -0,0 +1,6 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_13 {
+    // it returns as false!!
+
+}
---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_14_15_16.java
@@ -0,0 +1,5 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_14_15_16 {
+
+}
