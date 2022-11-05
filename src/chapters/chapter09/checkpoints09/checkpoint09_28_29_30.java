30ea9ff27054d37b5f2887365f3d787d7a23134a
etkaberk <etkaberk96@gmail.com>
2022-11-05 07:48:51 +0300

    ultimos

---
+++ src/chapters/chapter09/checkpoints09/checkpoint09_28_29_30.java
@@ -0,0 +1,7 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoint09_28_29_30 {
+}
+//09_28--> it is not a immutable class there must be no setters
+//09_29--> it is not a immutable class there must be no getters which returns the reference of data fields.
+//09_30--> it is not a immutable class because the getter method returns the reference of a data fields' variable .
---
+++ src/chapters/chapter09/checkpoints09/checkpoints09_32_33_34.java
@@ -0,0 +1,7 @@
+package chapters.chapter09.checkpoints09;
+
+public class checkpoints09_32_33_34 {
+    // 09_32 --> refering the obje and invoke a contructor from another contsructor
+    // 09_33 --> this keyword must be the first statement of the method and we can not reach the hidden data field without this keyword
+    // 09_34 --> the second method that use the class name can not reach the data field
+}
