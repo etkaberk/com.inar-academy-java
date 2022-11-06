7518f1a8835acd7841f5d259bbc1b0c69b26ea5e
etkaberk <etkaberk96@gmail.com>
2022-11-06 21:21:15 +0300

    beginChapter10

---
+++ src/chapters/Chapter10/Checkpoints10/Checkpoint10_01.java
@@ -0,0 +1,9 @@
+package chapters.Chapter10.Checkpoints10;
+
+public class Checkpoint10_01 {
+    /**
+     * there are 3 requirements for being immutable all data fields must be private and  there can't be any
+     * mutator methods for data fields and no accessor methods can return a reference to data field that is mutable !!!,
+     * so that the Loan Class cannot be immutable because of returning the reference of Date object.
+     */
+}
