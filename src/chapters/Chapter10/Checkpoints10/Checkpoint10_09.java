65970b1b255f7ddd217ac1d2488e30f01f32db54
etkaberk <etkaberk96@gmail.com>
2022-11-08 18:23:43 +0300

    new addition

--- src/chapters/Chapter10/Checkpoints10/Checkpoint10_09.java
+++ src/chapters/Chapter10/Checkpoints10/Checkpoint10_09.java
@@ -1,5 +1,14 @@
 package chapters.Chapter10.Checkpoints10;
 
+public class Checkpoint10_09 {}
+/**
 public class Checkpoint10_09 {
-    
-}
+    String s1 = 5 +"";
+    Integer int1 = Integer.parseInt(s1);
+    String string = 5.5 + "" ;
+    double st1 ;
+    String st = Double.toString(st1);
+    Double d = Double.parseDouble();
+   Double.valueOf(s1);
+    */
+
---
+++ src/chapters/Chapter10/Checkpoints10/Checkpoint10_10.java
@@ -0,0 +1,10 @@
+package chapters.Chapter10.Checkpoints10;
+
+public class Checkpoint10_10 {
+    public static void main(String[] args) {
+        Integer x = new Integer(3 );
+        System.out.println(x.intValue());
+        System.out.println(x.compareTo(new Integer(4)));// because smaller than the int(4)
+
+    }
+}
