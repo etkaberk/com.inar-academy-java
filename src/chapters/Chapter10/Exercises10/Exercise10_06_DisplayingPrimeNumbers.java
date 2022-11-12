5a8e30b7d998c89126e3faf8a239a5230d1f21ab
etkaberk <etkaberk96@gmail.com>
2022-11-12 17:42:55 +0300

    new solutions

--- src/chapters/Chapter10/Exercises10/Exercise10_06_DisplayingPrimeNumbers.java
+++ src/chapters/Chapter10/Exercises10/Exercise10_06_DisplayingPrimeNumbers.java
@@ -1,7 +1,35 @@
 package chapters.Chapter10.Exercises10;
 
-import java.util.Scanner;
+import chapters.Chapter10.Listings10.StackOfIntegers;
+
+
+
+import static chapters.chapter06.exercises06.exercise06_10.isPrime;
 
 public class Exercise10_06_DisplayingPrimeNumbers {
+    public static void main(String[] args) {
+        StackOfIntegers primeNumbers = findPrimeNumbers(120);
+        display(primeNumbers);
 
+    }
+
+    public static void display(StackOfIntegers list) {
+        while (!list.empty()) {
+            System.out.println(list.pop());
+        }
+    }
+
+    public static StackOfIntegers findPrimeNumbers(int i) {
+        int number = 2;
+        StackOfIntegers primeNumbers = new StackOfIntegers();
+        while (number <= i) {
+            if (isPrime(number)) {
+                primeNumbers.push(number);
+            }
+            number++;
+        }
+        return primeNumbers;
+
+
+    }
 }
